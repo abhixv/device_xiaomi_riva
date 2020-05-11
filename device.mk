0712b6c87c153f845c1e2437159cdc8de8305ee2
Nick <nikhilnick00007@gmail.com>
2020-05-11 08:06:07 +0000

    riva: make thermals pebuilt
    * X00RD blobs aren't working and its causing heat
    * olive's are working as they should
    
    Change-Id: Iee1a63e313815e9ce9d2bf62edb9346b05db39cf

--- device.mk
+++ device.mk
@@ -395,13 +395,6 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
-PRODUCT_PACKAGES += \
-    thermal.msm8937
-
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
